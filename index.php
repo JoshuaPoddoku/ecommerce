c519ab9890a3f7026ca92bfe9bd3934c3ed1a4a1
JOJO19 <31725457+JoshuaPoddoku@users.noreply.github.com>
2018-09-21 01:04:59 +0530

    Create index.php
    home page front end

---
+++ index.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+           
+        <!--jQuery library--> 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+        <!--Latest compiled and minified JavaScript--> 
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Lifestyle store</title>
+        <link  rel="stylesheet" href="style.css">
+        <link  rel="stylesheet" href="bootstrap.min.css">
+        
+        <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Open+Sans:300|Oswald|Raleway|Roboto" rel="stylesheet">
+    <body>
+        <nav class="navbar navbar-default "> 
+            
+                <div class="">
+                    <a class="navbar-brand" href="#">Lifestyle store</a>
+                </div>
+                <div class="myfont">
+                    <ul class="nav navbar-nav navbar-right">
+                        
+                        <li class="myfont"><a href="setting.html" class="navbaar-brand"  ><span class="glyphicon glyphicon-user">SignUp</span></a></li>
+                        <li class="myfont"><a href="logout.html" class="navbar-brand"   data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-out">LogIn</span></a></li>
+                    </ul>
+                </div>
+                
+                
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button> <!-- Close button for modal -->
+        <h4 class="modal-title">LOGIN</h4>
+      </div>
+      <div class="modal-body">
+                <form class="form-horizontal  ">
+                <p>Don't have account? <a href="#">Register</a></p>
+             
+               
+              <div class="col-sm-16 myfont">
+                <input type="email" class="form-control " id="email" placeholder="E-mail" name="email">
+               </div>
+                    
+                  <div class="col-sm-16 myfont">
+                 <input type="password" class="form-control " id="password" placeholder="Password" name="pws">
+                    </div>
+                        
+                    
+           
+            <button class="btn btn-info">Login</button>
+            
+            
+        </form>
+        <br>
+      <p><a href="#">Forgot Passoword?</a></p>
+      </div>
+      <div class="modal-footer">
+       
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+            
+        </nav>
+        
+<div class="container-fluid">
+           
+            <div class="row">
+               
+               
+                <div class="col-md-3 col-sm-6">
+                   <div class="panel panel-primary">
+                    <div class="panel-heading">Mobile 1</div>
+                       <div class="thumbnail">
+                        
+                    <div class="caption">
+                    <a href="#"><img src="img/nikon.jpg" ></a>
+                    
+                       <center>
+                        <h3 class="myfont">NIKON D7200</h3>
+                         <p>
+                            Rs. 50000
+                        </p>
+                        <button class="btn btn-info">Add to cart</button>
+                    </center>
+                        
+                    </div>
+                  
+                       
+                        
+                    </div> 
+                   
+                </div>
+                </div>
+    <div class="col-md-3 col-sm-6">
+                   <div class="panel panel-primary">
+                    <div class="panel-heading">Mobile 1</div>
+                       <div class="thumbnail">
+                        
+                    <div class="caption">
+                    <a href="#"><img src="img/nikon.jpg" ></a>
+                    
+                       <center>
+                        <h3 class="myfont">NIKON D7200</h3>
+                         <p>
+                            Rs. 50000
+                        </p>
+                        <button class="btn btn-info">Add to cart</button>
+                    </center>
+                        
+                    </div>
+                  
+                       
+                        
+                    </div> 
+                   
+                </div>
+                </div>
+                
+                
+                   <div class="col-md-3 col-sm-6">
+                   <div class="panel panel-primary">
+                    <div class="panel-heading">Mobile 1</div>
+                       <div class="thumbnail">
+                        
+                    <div class="caption">
+                    <a href="#"><img src="img/nikon.jpg" ></a>
+                    
+                       <center>
+                        <h3 class="myfont">NIKON D7200</h3>
+                         <p>
+                            Rs. 50000
+                        </p>
+                        <button class="btn btn-info">Add to cart</button>
+                    </center>
+                        
+                    </div>
+                  
+                       
+                        
+                    </div> 
+                   
+                </div>
+                </div>
+                    <div class="col-md-3 col-sm-6">
+                   <div class="panel panel-primary">
+                    <div class="panel-heading">Mobile 1</div>
+                       <div class="thumbnail">
+                        
+                    <div class="caption">
+                    <a href="#"><img src="img/nikon.jpg" ></a>
+                    
+                       <center>
+                        <h3 class="myfont">NIKON D7200</h3>
+                         <p>
+                            Rs. 50000
+                        </p>
+                        <button class="btn btn-info">Add to cart</button>
+                    </center>
+                        
+                    </div>
+                  
+                       
+                        
+                    </div> 
+                   
+                </div>
+                </div>
+            </div>
+            <div class="row">
+                  <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        
+                    <div class="caption myfont">
+                    <a href="#"><img src="img/casio.jpg" ></a>
+                    
+                       <center>
+                        <h3 class="myfont">CASIO G-SHOCK</h3>
+                         <p>
+                            Rs. 10000
+                        </p>
+                        <button class="btn btn-info">Add to cart</button>
+                    </center>
+                    </div>
+                    </div> 
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        
+                    <div class="caption myfont">
+                    <a href="#"><img src="img/th.jpg" ></a>
+                    
+                       <center>
+                        <h3 class="myfont">TOMMY HILFIGER</h3>
+                         <p>
+                            Rs. 7990
+                        </p>
+                        <button class="btn btn-info">Add to cart</button>
+                    </center>
+                    </div>
+                    </div> 
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        
+                    <div class="caption myfont">
+                    <a href="#"><img src="img/armani.jpg" ></a>
+                    
+                       <center>
+                        <h3 class="myfont">EMPORIO ARMANI</h3>
+                         <p>
+                            Rs. 37600
+                        </p>
+                        <button class="btn btn-info">Add to cart</button>
+                    </center>
+                    </div>
+                    </div> 
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        
+                    <div class="caption myfont">
+                    <a href="#"><img src="img/fossil.jpg" ></a>
+                    
+                       <center>
+                        <h3 class="myfont">FOSSIL</h3>
+                         <p>
+                            Rs. 8990
+                        </p>
+                        <button class="btn btn-info">Add to cart</button>
+                    </center>
+                    </div>
+                    </div> 
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        
+                    <div class="caption myfont">
+                    <a href="#"><img src="img/uspa.jpg" ></a>
+                    
+                       <center>
+                        <h3 class="myfont">U.S. POLO ASSN.</h3>
+                         <p>
+                            Rs. 1999
+                        </p>
+                        <button class="btn btn-info">Add to cart</button>
+                    </center>
+                    </div>
+                    </div> 
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        
+                        .0
+                    <div class="caption myfont">
+                    <a href="#"><img src="img/jnj.jpg" ></a>
+                    
+                       <center>
+                        <h3 class="myfont">JACK AND JONES</h3>
+                         <p>
+                            Rs. 2900
+                        </p>
+                        <button class="btn btn-info">Add to cart</button>
+                    </center>
+                    </div>
+                    </div> 
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        
+                    <div class="caption myfont">
+                    <a href="#"><img src="img/ucb.jpg" ></a>
+                    
+                       <center>
+                        <h4 class="myfont">UNITED COLORS OF BENNETON</h4>
+                         <p>
+                            Rs. 2500
+                        </p>
+                        <button class="btn btn-info">Add to cart</button>
+                    </center>
+                    </div>
+                    </div> 
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        
+                    <div class="caption myfont">
+                    <a href="#"><img src="img/wrogn.jpg" ></a>
+                    
+                       <center>
+                        <h3 class="myfont">WROGN</h3>
+                         <p>
+                            Rs. 1300
+                        </p>
+                        <button class="btn btn-info">Add to cart</button>
+                    </center>
+                    </div>
+                    </div> 
+                </div>
+                
+            </div>
+            
+            
+        </div>
+       
+        
+      
+        
+        
+    </div>
+           
+            <footer class="myfont myfooter">
+                <center>
+                <p>Copyright &COPY; Lifestyle Store. All Rights Reserved | Contact Us: +91 90000 00000</p>
+                </center>
+            </footer>
+        
+        
+            
+    </body>
+</html>
